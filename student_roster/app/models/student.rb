b0f298b3967307a92cb543c7e714d58a10ced9cf
Jerin Hiatt <jerinhiatt@gmail.com>
2016-04-06 08:19:03 -0600

    Added email validation

--- student_roster/app/models/student.rb
+++ student_roster/app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, :format => /.+@.+\..+/i
   validates :cell_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please use ###-###-####" }
   validates :home_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please use ###-###-####" }
   validates :work_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please use ###-###-####" }
